0dee2574434d7702e4e0afebd77d7e892a634aae
KseniaShchekoturova <igoshevakp@gmail.com>
2018-08-13 11:57:24 +0400

    homework - part 1

--- script AB.js
+++ script AB.js
@@ -1,17 +1,4 @@
-function countWords(str) {
-  if (str==''){return 0}
-  var arr = str.split(' ');
-  var i = 0;
-  while (i<arr.length){
-    if (arr[i].replace(' ','') == ''){
-		arr.splice(i, 1);  
-    }else{
-		i++;
-	}
-  }
-  return arr.length;
-}
-
+// 1.	Подсчет Букв
 function countChar(Str,symb){
   var entrNum = 0, i = 0;
   N = Str.length;
@@ -22,8 +9,9 @@ function countChar(Str,symb){
 	i++;
   }
   return(entrNum);
-}
+};
 
+// 2.	Глубокое сравнение
 function deepCompare_old(Value1,Value2){
 	// проверка на типы
 	if (typeof(Value1) != typeof(Value2)){
@@ -45,14 +33,15 @@ function deepCompare_old(Value1,Value2){
 	
 	// проверка значений свойств
 	for(var key in Value1) {
-		if (Value1[key] != Value2[key]){
+		if (Value1[key] !== Value2[key]){
 			return false;
 		}
 	}
 	
 	return true;
-}
+};
 
+//2.	Глубокое сравнение
 function deepCompare(Value1,Value2){
 	// проверка на типы
 	if (typeof(Value1) != typeof(Value2)){
@@ -65,14 +54,15 @@ function deepCompare(Value1,Value2){
 			return sr;
 		}
 	}else {
-		if(Value1 != Value2){
+		if(Value1 !== Value2){
 			return false;
 		}
 	}
 	
 	return true;
-}
+};
 
+// 3.	Шахматная доска
 function chessBoard(w,h){
   var i, j, Str = '', ch = false;
   for (i = 0; i < h; i++) {
@@ -92,8 +82,9 @@ function chessBoard(w,h){
 	}
   }
   return(Str);
-}
+};
 
+// 4.	Диапазон
 function makeArray(elStart,elFinish,step){
 	if (elStart == undefined 
 	| elFinish == undefined){
@@ -127,8 +118,9 @@ function makeArray(elStart,elFinish,step){
 	}
 	
 	return(Arr);
-}
+};
 
+// 5.	Наоборот
 function reverseArray(Arr){
 	var i = 0, newArr = [];
 	L = Arr.length;
@@ -139,7 +131,7 @@ function reverseArray(Arr){
 	}
 	
 	return(newArr);
-}
+};
 
 function reverseArrayInPlace(Arr){
 	var i = 0, val;
@@ -153,8 +145,9 @@ function reverseArrayInPlace(Arr){
 	}
 	
 	return(Arr);
-}
+};
 
+// 6.	Свертка
 function mergeArrays(Arr1,Arr2){
 	var TotalArr = Arr1.concat(Arr2);
 	
@@ -167,8 +160,89 @@ function mergeArrays(Arr1,Arr2){
 	}
 
 	return TotalArr;
-}
+};
 
-function rent(DaysNum){
-	DaysNum / 7;
+// 7.	Every и some
+function everyIsNaN(Arr){
+	var i;
+	for (i = 0; i < Arr.length; i++){
+		if (!isNaN(Arr[i])){
+			return false;
+		}
+	}
+	return true;
+};
+
+function someIsNaN(Arr){
+	var i;
+	for (i = 0; i < Arr.length; i++){
+		if (isNaN(Arr[i])){
+			return true;
+		}
+	}
+	return false;
+};
+
+// 8.	Повтор
+function multiplyOrThrow(a, b) {
+  if (Math.random() < 0.5) {
+    return a * b;
+  } else {
+    throw 'MultiplicatorUnitFailure';
+  }
+};
+
+function multiply(a,b){
+	try{
+		return multiplyOrThrow(a, b);
+	}catch{
+		return multiply(a, b);
+	}
+};
+
+// 9.	Кавычки в тексте
+
+// 10.	Найти числа
+
+// 11.	День и месяц
+function getNames(date){
+	//dateStr = date.toString();
+	//var arr = dateStr.split(' ');
+	//return arr[1] + ', ' + arr[0];
+	
+	var Month;
+	switch(date.getMonth()){
+		case 0:Month = 'January';break;
+		case 1:Month = 'February';break;
+		case 2:Month = 'March';break;
+		case 3:Month = 'April';break;
+		case 4:Month = 'May';break;
+		case 5:Month = 'June';break;
+		case 6:Month = 'July';break;
+		case 7:Month = 'August';break;
+		case 8:Month = 'September';break;
+		case 9:Month = 'October';break;
+		case 10:Month = 'November';break;
+		case 11:Month = 'December';break;
+	}
+	
+	var WeekDay;
+	switch(date.getDay()){
+		case 0:WeekDay = 'Sunday';break;
+		case 1:WeekDay = 'Monday';break;
+		case 2:WeekDay = 'Tuesday';break;
+		case 3:WeekDay = 'Wednesday';break;
+		case 4:WeekDay = 'Thursday';break;
+		case 5:WeekDay = 'Friday';break;
+		case 6:WeekDay = 'Saturday';break;
+	}
+	
+	return Month + ', ' + WeekDay;
+};
+
+// 12.	Разница в годах
+function differenceInYears(dateStart,dateFinish){
+	daysBetween = (dateFinish - dateStart) / (24*3600*1000);
+	yearsBeetween = Math.round(10 * daysBetween / 365)/10;
+	return yearsBeetween;
 }
