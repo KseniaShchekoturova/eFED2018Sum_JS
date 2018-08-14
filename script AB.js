5d877208b5319da67f6141df8d534cc3fe695f33
KseniaShchekoturova <igoshevakp@gmail.com>
2018-08-14 10:45:04 +0400

    edits

--- script AB.js
+++ script AB.js
@@ -50,19 +50,20 @@ function deepCompare(Value1,Value2){
 	}
 	
 	if (typeof Value1 == 'object'){
-		for(var key in Value1) {
-			equal = deepCompare(Value1[key],Value2[key]);
-			if (!equal){
-				return false;
-			};
-		}
-		return true;
+		// проверка на количество свойств объектов
+		if (Object.keys(Value1).length != Object.keys(Value2).length){return false};
+		// проверка на имена свойств объектов и значения свойств объектов
+		try{
+			for(var key in Value1){
+				equal = deepCompare(Value1[key],Value2[key]);
+				if (!equal){return false;};
+			}
+		}catch{return false}
 	}else {
-		if(Value1 !== Value2){
-			return false;
-		}
+		if(Value1 !== Value2){return false;}
 	}
 	
+	// если нигде не свалилось, то 
 	return true;
 };
 
